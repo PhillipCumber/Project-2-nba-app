54af499d14db3b9b0db570464008a0fdc7383e10
Phillip Cumberlander <cumberlander11@gmail.com>
2018-10-25 15:34:18 -0400

    properly seeded my database

--- db/seeds.js
+++ db/seeds.js
@@ -4,3 +4,33 @@ let mongoose = require('./connection')
 ///Imported team model
 let Lakers = require('../models/Lakers')
 
+///Custom Information
+let LakersPlayers = [
+
+    {
+        name: "Lebron James",
+        number: 23,
+        team: "Los Angeles Lakers"
+    },
+
+    {
+        name: "Rajon Rondo",
+        number: 9,
+        team: "Los Angeles Lakers"
+    },
+
+    {
+        name: "Kyle Kuzma",
+        number: 0,
+        team: "Los Angeles Lakers"
+    }
+]
+
+///Create method
+Lakers.create(LakersPlayers)
+    .then(lakers => {
+
+        console.log('New Player', lakers)
+    })
+
+
