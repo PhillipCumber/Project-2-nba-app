ef811479705ca0b0dce5a4840c8ea88a95ee3b4d
Phillip Cumberlander <cumberlander11@gmail.com>
2018-10-29 12:03:23 -0400

    test

--- db/seeds.js
+++ db/seeds.js
@@ -47,7 +47,9 @@ Lakers.create(LakersPlayers)
             bestPlayer: 'Lebron James',
             rings: 17,
             img: 'https://i.ebayimg.com/images/g/ylAAAOxyyF5RP7b9/s-l300.jpg',
-            coach: 'Luke Walton'
+            coach: 'Luke Walton',
+            website: 'https://www.nba.com/lakers/',
+
         },
 
         {
@@ -56,7 +58,8 @@ Lakers.create(LakersPlayers)
             bestPlayer: 'Trae Young',
             rings: 1,
             img: 'https://s3.amazonaws.com/s3.wzgcfm.radio.com/altanta_hawks.png',
-            coach: 'Lloyd Pierce'
+            coach: 'Lloyd Pierce',
+            website: 'https://www.nba.com/lakers/',
         },
 
         {
@@ -65,7 +68,8 @@ Lakers.create(LakersPlayers)
             bestPlayer: 'Demar Derozan',
             rings: 5,
             img: 'https://pbs.twimg.com/profile_images/991108132876124160/AvXftxBU_400x400.jpg',
-            coach: 'Greg Poppivich'
+            coach: 'Greg Poppivich',
+            website: 'https://www.nba.com/lakers/',
         },
 
         {
@@ -74,7 +78,8 @@ Lakers.create(LakersPlayers)
             bestPlayer: 'Kristaps Porzingis',
             rings: 1,
             img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtoTDfrFjLvcD1Y_AEirnfUa3XxgZT8JPdE6U9NQCPtVywNjRi',
-            coach: 'David Fizdale'
+            coach: 'David Fizdale',
+            website: 'https://www.nba.com/lakers/',
         },
 
     ]
