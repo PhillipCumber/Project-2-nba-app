70cc278e824b7bdbe081e6755526132dc793adc5
Phillip Cumberlander <cumberlander11@gmail.com>
2018-10-29 10:32:10 -0400

    test number 6 to try to get heroku to open

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,23 @@
-///Declared mongoose module
-let mongoose = require('mongoose')
+// require('dotenv').config()
+// ///Declared mongoose module
+// let mongoose = require('mongoose')
 
-///Connecting to local server
-mongoose.connect('mongodb://localhost/nba')
+// ///Connecting to local server
+// mongoose.connect('mongodb://localhost/nba')
 
-mongoose.connect('open', () => {
+// mongoose.connect('open', () => {
 
-    console.log('Mongoose is connected')
+//     console.log('Mongoose is connected')
+// })
+
+
+// module.exports = mongoose
+
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+   console.log('Connected to MongoDB')
 })
-
 
 module.exports = mongoose
--- server.js
+++ server.js
@@ -26,3 +26,5 @@ app.listen(PORT, () => {
 
     console.log(`Server is listening on PORT: ${PORT}`)
 })
+
+module.exports = app
